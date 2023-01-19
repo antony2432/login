05b20a19a87c2a4a2494549da2dcdd4b5ed6a3d7
antony24 <achuquival@gmail.com>
2023-01-19 10:46:01 -0500

    agrando todos los cambios para el dp

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -3,26 +3,29 @@ import { serialize } from "cookie"
 
 export default function loginHandler(req, res) {
   const { email, password } = req.body
-  console.log(req.body)
 
-  if (email === 'antony@conectica.com' && password === 'admin') {
-    const token = jwt.sign({
-      exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30,
-      email: 'antony@conectica.com',
-      username: 'antony Chuquival'
-    }, 'secret',)
+  try {
+    if (email === 'user@dominio.com' && password === 'admin') {
+      const token = jwt.sign({
+        exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30,
+        email: 'user@dominio.com',
+        username: 'user name'
+      }, 'secret',)
 
-    const serialized = serialize('myTokenName', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== 'production',
-      sameSite: 'none', // none para cuando te comunicas con un backend externo
-      maxAge: 1000 * 60 * 60 * 24 * 30,
-      path: '/'
-    })
-    console.log('llego aqui, antes de enviar por header')
-    res.setHeader('Set-Cookie', serialized)
-    console.log('llego aqui')
-    return res.json('login success', { token })
+      const serialized = serialize('myTokenName', token, {
+        httpOnly: true,
+        secure: false,
+        sameSite: 'none',
+        maxAge: 1000 * 60 * 60 * 24 * 30,
+        path: '/'
+      })
+      console.log(serialized)
+      res.setHeader('Set-Cookie', serialized)
+      console.log('login success')
+      return res.json('login success', { token })
+    }
+  } catch (error) {
+    console.log(error)
   }
 
   res.status(401).json({ error: 'Invalid credentials' })
